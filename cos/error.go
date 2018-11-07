d00ff41b69c9c5f658afda80638f02d8f9bf2461
roc <rockerchen@tencent.com>
2018-11-07 17:26:54 +0800

    add IsNotFoundError

--- cos/error.go
+++ cos/error.go
@@ -14,3 +14,11 @@ type Error struct {
 func (e *Error) Error() string {
 	return fmt.Sprintf("COS API Error: RequestId: %s Status Code: %d Code: %s Message: %s", e.RequestId, e.StatusCode, e.Code, e.Message)
 }
+
+// IsNotFoundError return true if the error is 404 error
+func IsNotFoundError(err error) bool {
+	if e, ok := err.(*Error); ok && e.StatusCode == 404 {
+		return true
+	}
+	return false
+}
